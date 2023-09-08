88b0adb4ca798e31ec9f964672454b7892e0861d
andres briñez <brinezlopez08@gmail.com>
2023-09-08 12:55:22 -0500

    Creación entidad de dominio Cuenta

--- src/main/java/com/ProyectoIntegrador/sistematransaccionesbancarias/domain/entities/Cuenta.java
+++ src/main/java/com/ProyectoIntegrador/sistematransaccionesbancarias/domain/entities/Cuenta.java
@@ -1,4 +1,72 @@
 package com.ProyectoIntegrador.sistematransaccionesbancarias.domain.entities;
 
+import java.sql.Date;
+
 public class Cuenta {
+    private Integer id;
+    private String numeroCuenta;
+    private Date fechaCreacion;
+    private Double saldo;
+    private Double metaAhorro;
+    private Usuario usuario_id;
+
+    public Cuenta(){
+    }
+
+    public Cuenta(Integer id, String numeroCuenta, Date fechaCreacion, Double saldo, Double metaAhorro, Usuario usuario_id) {
+        this.id = id;
+        this.numeroCuenta = numeroCuenta;
+        this.fechaCreacion = fechaCreacion;
+        this.saldo = saldo;
+        this.metaAhorro = metaAhorro;
+        this.usuario_id = usuario_id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Double getMetaAhorro() {
+        return metaAhorro;
+    }
+
+    public void setMetaAhorro(Double metaAhorro) {
+        this.metaAhorro = metaAhorro;
+    }
+
+    public Usuario getUsuario_id() {
+        return usuario_id;
+    }
+
+    public void setUsuario_id(Usuario usuario_id) {
+        this.usuario_id = usuario_id;
+    }
 }
