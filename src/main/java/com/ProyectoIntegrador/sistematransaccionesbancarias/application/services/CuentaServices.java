7b06ef0af29c640ab644e3c99e183629771750c8
andres briñez <brinezlopez08@gmail.com>
2023-09-08 13:14:32 -0500

    Crear servicio para obtener, guardar y actualizar una cuenta

--- src/main/java/com/ProyectoIntegrador/sistematransaccionesbancarias/application/services/CuentaServices.java
+++ src/main/java/com/ProyectoIntegrador/sistematransaccionesbancarias/application/services/CuentaServices.java
@@ -1,4 +1,34 @@
 package com.ProyectoIntegrador.sistematransaccionesbancarias.application.services;
 
+import com.ProyectoIntegrador.sistematransaccionesbancarias.domain.entities.Cuenta;
+import com.ProyectoIntegrador.sistematransaccionesbancarias.domain.entities.Usuario;
+import com.ProyectoIntegrador.sistematransaccionesbancarias.domain.repositories.CuentaRepository;
+
 public class CuentaServices {
+
+    // inyección de dependencias
+    CuentaRepository cuentaRepository;
+
+    public CuentaServices(CuentaRepository cuentaRepository) {
+        this.cuentaRepository = cuentaRepository;
+    }
+
+    public boolean saveOrUpdateCuenta(Cuenta cuenta) {
+
+        Usuario usuarioCuenta = cuenta.getUsuario_id(); // obtiene el usuario de la cuenta
+        Integer idUsuario = usuarioCuenta.getId(); // obtiene el id del usuario de la cuenta
+
+        cuentaRepository.saveOrUpdateCuenta(cuenta);
+        if (cuentaRepository.getCuentaByIdUsuario(idUsuario) != null) // si al buscar el usuario con el id del usuario, entonces se creó o actualizó correctamente
+            return true;
+        return true;
+    }
+
+    public Cuenta getCuentaByIdUsuario(Integer idUsuario) {
+        return cuentaRepository.getCuentaByIdUsuario(idUsuario);
+    }
+
+
+
+
 }
