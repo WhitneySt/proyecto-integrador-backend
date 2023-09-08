5e06dc2f5b93f80f00bc457842f4df542f324608
andres briñez <brinezlopez08@gmail.com>
2023-09-08 12:59:53 -0500

    Crear repositoria de Cuenta

--- src/main/java/com/ProyectoIntegrador/sistematransaccionesbancarias/domain/repositories/CuentaRepository.java
+++ src/main/java/com/ProyectoIntegrador/sistematransaccionesbancarias/domain/repositories/CuentaRepository.java
@@ -1,4 +1,10 @@
 package com.ProyectoIntegrador.sistematransaccionesbancarias.domain.repositories;
 
+import com.ProyectoIntegrador.sistematransaccionesbancarias.domain.entities.Cuenta;
+
 public interface CuentaRepository {
+
+    public boolean saveOrUpdateCuenta(Cuenta cuenta);
+    public Cuenta getCuentaByIdUsuario(Integer idUsuario);
+
 }
