221cba5261c57b666c36e390502c7d7656adb9de
andres briñez <brinezlopez08@gmail.com>
2023-09-14 20:32:36 -0500

    Se quita el bloqueo de la seguridad

--- src/main/java/com/ProyectoIntegrador/sistematransaccionesbancarias/SistemaTransaccionesBancariasApplication.java
+++ src/main/java/com/ProyectoIntegrador/sistematransaccionesbancarias/SistemaTransaccionesBancariasApplication.java
@@ -2,8 +2,9 @@ package com.ProyectoIntegrador.sistematransaccionesbancarias;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class SistemaTransaccionesBancariasApplication {
 
 	public static void main(String[] args) {
