eda74e9650a4017a6f56749c2128572b3c6b0add
andres briñez <brinezlopez08@gmail.com>
2023-09-14 20:33:51 -0500

    Se  hace la autenticación del usuario por  un formulario de  login

--- src/main/java/com/ProyectoIntegrador/sistematransaccionesbancarias/infraestructure/security/SecurityConfig.java
+++ src/main/java/com/ProyectoIntegrador/sistematransaccionesbancarias/infraestructure/security/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.ProyectoIntegrador.sistematransaccionesbancarias.infraestructure.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
@@ -17,14 +21,9 @@ public class SecurityConfig {
     @Autowired
     private DataSource dataSource;  // Copia virtual de la base de datos para poder hacer consultas y no ir directamente a la base de datos, esto es más eficiente
 
-
     // Cuando el usuario intente iniciar sesión se debe hacer una consulta a la base de datos para verificar que el usuario exista
-    // y que la contraseña sea la correcta, para esto se debe crear un objeto de tipo JdbcUserDetailsManager que es el que se encarga de hacer la consulta a la base de datos
-    // y verificar que el usuario exista y que la contraseña sea la correcta
-    // Para crear el objeto JdbcUserDetailsManager se debe crear un objeto de tipo DataSource que es el que se encarga de hacer la conexión con la base de datos
 
     //? Validar la existencia del usuario, codificar la contraseña y verificar que coincida con la contraseña encriptada de la base de datos ( hace la autenticacion de los usuarios)
-
     @Autowired
     public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
         try{
@@ -41,6 +40,61 @@ public class SecurityConfig {
             System.out.println(e.getMessage());
             System.out.println("Error en la consulta de usuarios");
         }
+    }
+
+    @Bean // Permite que el objeto retornado por el método se administre por el contenedor de Spring
+    public SecurityFilterChain securiryFilterChain (HttpSecurity http ) throws Exception {
+
+        return http // retorna un objeto SecurityFilterChain que se encarga de configurar la seguridad web en la aplicación
+                .csrf(csrf-> csrf.disable()) // Deshabilita la protección CSRF
+
+                // Autorizacion de los endpoints
+                .authorizeHttpRequests(authRaquests ->
+                                authRaquests
+                                        .requestMatchers("/login","/registro").permitAll() // los endpoints que empiecen con  /registro y login son publicos y no requieren autenticacion
+                                        .anyRequest().permitAll() // cualquier otra ruta es publica y no requiere autenticacion
+                )
+
+                //.formLogin(Customizer.withDefaults()) // el login por defecto es el que viene por defecto de spring
+                .formLogin(formLogin -> // Se configura el formulario de inicio de sesión personalizado
+                        formLogin
+                                .loginPage("/login") // Se especifica la ruta del formulario de inicio de sesión personalizado
+                                .permitAll() // Se permite el acceso a todos los usuarios
+                                .defaultSuccessUrl("/home", true) // Se especifica la ruta a la que se redirigirá al usuario después de iniciar sesión correctamente
+
+                                //.failureUrl("/login?error=true") // Se especifica la ruta a la que se redirigirá al usuario después de iniciar sesión incorrectamente
+                                .failureHandler((request, response, exception) -> {
+                                    // en el caso de que el usuario este deshabilitado
+                                    if (exception instanceof DisabledException) { // si la excepcion es de tipo DisabledException es porque el usuario esta deshabilitado
+                                       // request.getSession().setAttribute("mensaje", "Usuario deshabilitado"); //  establece un atributo llamado "mensaje" en la sesión de la solicitud actual.
+                                        response.sendRedirect("/login?deshabilitado=true");
+                                    }
+                                    // si hay algun error aparece esta url, y desde la plantilla se genera un error
+                                    else {
+                                        //request.getSession().setAttribute("mensaje", "Credenciales de inicio de sesión incorrectas"); // se comentó para no enviarla a la plantilla y ver otra forma de generar mensajes
+                                        response.sendRedirect("/login?error=true");
+                                    }
+                                })
+                                // estos dos son los campos que se deben enviar en el formulario de login, por eso debe tener el nombre del input de cada uno
+                                .usernameParameter("id") // Se especifica el nombre del parámetro que se utilizará para obtener el nombre de usuario
+                                .passwordParameter("contrasena") // Se especifica el nombre del parámetro que se utilizará para obtener la contraseña
+
+                                // TODO Habilitar cuando se haga autoriazacion por roles
+                                // .successHandler((AuthenticationSuccessHandler) customSuccessHandler) // se envia el manejador de exito para que se ejecute cuando se inicie sesion correctamente para que se haga la redireccion dependiendo del rol
+                )
+
+                .logout(logout -> // Se configura el formulario de cierre de sesión personalizado
+                        logout
+                                .logoutUrl("/logout") // Se especifica la ruta del formulario de cierre de sesión personalizado
+                                .logoutSuccessUrl("/login") // Se especifica la ruta a la que se redirigirá al usuario después de cerrar sesión correctamente
+                                .permitAll() // Se permite el acceso a todos los usuarios
+                                .invalidateHttpSession(true) // invalida la sesion
+                                .deleteCookies("JSESSIONID") // elimina la cookie de la sesion
+                )
+
+                .build(); // Se construye el objeto SecurityFilterChain
+
+
     }
 
 
