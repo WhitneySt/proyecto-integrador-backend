01d34f49cf6e2b716263a3f551f2e860e0ccaae5
andres briñez <brinezlopez08@gmail.com>
2023-09-18 22:17:50 -0500

    Se cambia que el método getCuentaByIdUsuario retorna un optional

--- src/main/java/com/ProyectoIntegrador/sistematransaccionesbancarias/domain/repositories/CuentaRepository.java
+++ src/main/java/com/ProyectoIntegrador/sistematransaccionesbancarias/domain/repositories/CuentaRepository.java
@@ -2,10 +2,12 @@ package com.ProyectoIntegrador.sistematransaccionesbancarias.domain.repositories
 
 import com.ProyectoIntegrador.sistematransaccionesbancarias.domain.entities.Cuenta;
 
+import java.util.Optional;
+
 public interface CuentaRepository {
 
     public boolean saveOrUpdateCuenta(Cuenta cuenta);
-    public Cuenta getCuentaByIdUsuario(Integer idUsuario);
+    public Optional<Cuenta> getCuentaByIdUsuario(Integer idUsuario);
 
 //     public boolean transferencia(Integer idCuentaOrigen, Integer idCuentaDestino, Double monto);
 
