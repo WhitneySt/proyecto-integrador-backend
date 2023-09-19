3ab5730447a3781eec9af8d6a37b0da7e1e92f33
andres briñez <brinezlopez08@gmail.com>
2023-09-18 22:15:53 -0500

    Se crea el método que permite obtejer la cuenta del usuario

--- src/main/java/com/ProyectoIntegrador/sistematransaccionesbancarias/infraestructure/data/jpaRepositories/cuenta/CuentaJPARepository.java
+++ src/main/java/com/ProyectoIntegrador/sistematransaccionesbancarias/infraestructure/data/jpaRepositories/cuenta/CuentaJPARepository.java
@@ -1,11 +1,18 @@
 package com.ProyectoIntegrador.sistematransaccionesbancarias.infraestructure.data.jpaRepositories.cuenta;
 
+import com.ProyectoIntegrador.sistematransaccionesbancarias.domain.entities.Cuenta;
 import com.ProyectoIntegrador.sistematransaccionesbancarias.infraestructure.data.dbo.CuentaJPAEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CuentaJPARepository extends CrudRepository<CuentaJPAEntity,Long>{
 
     // hay que traer la cuenta por id de un usuario
+    // consulta sql
+    @Query(value = "SELECT * FROM cuentas WHERE usuario_id = ?1", nativeQuery = true)
+    Optional<CuentaJPAEntity> findByUsuarioId(Integer usuarioId);
 }
