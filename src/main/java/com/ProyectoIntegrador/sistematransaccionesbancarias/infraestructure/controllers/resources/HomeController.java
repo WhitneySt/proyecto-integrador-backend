da77bfe4c5a30176db8d244ec4a3de0d012298a5
andres briñez <brinezlopez08@gmail.com>
2023-09-19 20:59:18 -0500

    Se crea el controlador para ver la información de la cuenta o se puede crear una cuenta

--- src/main/java/com/ProyectoIntegrador/sistematransaccionesbancarias/infraestructure/controllers/resources/HomeController.java
+++ src/main/java/com/ProyectoIntegrador/sistematransaccionesbancarias/infraestructure/controllers/resources/HomeController.java
@@ -1,12 +1,16 @@
 package com.ProyectoIntegrador.sistematransaccionesbancarias.infraestructure.controllers.resources;
 
+import com.ProyectoIntegrador.sistematransaccionesbancarias.application.services.BolsilloServices;
 import com.ProyectoIntegrador.sistematransaccionesbancarias.application.services.CuentaServices;
 import com.ProyectoIntegrador.sistematransaccionesbancarias.domain.entities.Cuenta;
 import com.ProyectoIntegrador.sistematransaccionesbancarias.domain.entities.Usuario;
 import com.ProyectoIntegrador.sistematransaccionesbancarias.infraestructure.controllers.dto.CuentaDto;
+import com.ProyectoIntegrador.sistematransaccionesbancarias.infraestructure.data.jpaRepositories.bolsillo.BolsilloImplementacion;
+import com.ProyectoIntegrador.sistematransaccionesbancarias.infraestructure.data.jpaRepositories.bolsillo.BolsilloJPARepository;
 import com.ProyectoIntegrador.sistematransaccionesbancarias.infraestructure.data.jpaRepositories.cuenta.CuentaImplementacion;
 import com.ProyectoIntegrador.sistematransaccionesbancarias.infraestructure.data.jpaRepositories.cuenta.CuentaJPARepository;
 import com.ProyectoIntegrador.sistematransaccionesbancarias.infraestructure.exepciones.CuentaNotFoundException;
+import com.ProyectoIntegrador.sistematransaccionesbancarias.infraestructure.mapper.MapperBolsillo;
 import com.ProyectoIntegrador.sistematransaccionesbancarias.infraestructure.mapper.MapperCuenta;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,33 +29,53 @@ public class HomeController {
 
     @Autowired
     MapperCuenta mapperCuenta;
+    MapperBolsillo mapperBolsillo;
     static CuentaServices cuentaServices;
     CuentaImplementacion repository;
+    BolsilloServices bolsilloServices;
+    BolsilloImplementacion bolsilloRepository;
 
     @Autowired
-    public HomeController(CuentaJPARepository cuentaJPARepository, MapperCuenta mapperCuenta) {
+    public HomeController(CuentaJPARepository cuentaJPARepository, BolsilloJPARepository bolsilloJPARepository,MapperCuenta mapperCuenta,MapperBolsillo mapperBolsillo ) {
         this.mapperCuenta = mapperCuenta;
+        this.mapperBolsillo = mapperBolsillo;
+
         this.repository = new CuentaImplementacion(cuentaJPARepository,mapperCuenta);
+        this.bolsilloRepository = new BolsilloImplementacion(bolsilloJPARepository,mapperBolsillo);
+
         this.cuentaServices = new CuentaServices(this.repository);
+        this.bolsilloServices = new BolsilloServices(this.bolsilloRepository);
     }
 
     @GetMapping({"/home", "/"})
     public String home(Model model,HttpServletRequest request,@ModelAttribute("mensaje") String mensajeRecibido) {
 
+        Boolean isCuentaCreada = false;
+
         Usuario usuarioLogeado = getUsuarioLogeado(request); // Se obtiene el usuario que inició sesión
         model.addAttribute("nombreUsuario", usuarioLogeado.getNombre()); // Se agrega el nombre del usuario al modelo para poder usarlo en la vista
 
+        CuentaDto cuentaDto = new CuentaDto();
+        
         try{
+
             Cuenta cuenta = cuentaServices.getCuentaByIdUsuario(usuarioLogeado.getId()); // Se obtiene la cuenta del usuario logeado
-            System.out.println("Cuenta del usuario logeado: "+cuenta.getMetaAhorro());
+
+            Double totalSaldoBolsillos= bolsilloServices.getTotalSaldoBolsillos(cuenta.getId()); // Se obtiene el saldo disponible del usuario logeado
+            Double saldoDisponible= cuenta.getSaldo()-totalSaldoBolsillos; // Se calcula el saldo disponible del usuario logeado
+
+            model.addAttribute("cuenta", cuenta); // Se agrega la cuenta al modelo para poder usarlo en la vista
+            model.addAttribute("saldoDisponible", saldoDisponible); // Se agrega el saldo disponible al modelo para poder usarlo en la vista
+
+            isCuentaCreada = true;
         }
         catch(CuentaNotFoundException e){ // Si no se encuentra la cuenta del usuario logeado se puede crear una
-            System.out.println(e.getMessage());
 
-            CuentaDto cuentaDto = new CuentaDto();
+            isCuentaCreada = false;
             model.addAttribute("cuentaDto", cuentaDto); //se guarda un objeto en el  modelo para poder usarlo en la vista y guardar valores
 
         }
+        model.addAttribute("isCuentaCreada", isCuentaCreada); // Se agrega la variable isCuentaCreada al modelo para poder usarlo en la vista
 
         return "/home";
     }
