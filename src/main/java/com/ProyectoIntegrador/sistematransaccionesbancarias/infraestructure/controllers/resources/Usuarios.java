d036ba60d45f3e9e7388e87bd6da8ae8dbbe64c0
andres briñez <brinezlopez08@gmail.com>
2023-09-22 23:33:23 -0500

    Se crea dos métodos para ver el detalle de un usuario registrado

--- src/main/java/com/ProyectoIntegrador/sistematransaccionesbancarias/infraestructure/controllers/resources/Usuarios.java
+++ src/main/java/com/ProyectoIntegrador/sistematransaccionesbancarias/infraestructure/controllers/resources/Usuarios.java
@@ -10,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -40,9 +43,35 @@ public class Usuarios {
         return "users/users";
     }
 
-    @GetMapping("/detalleUsuario")
-    public String verDetalle(Model model, HttpServletRequest request) {
 
+    /* ?
+    * Se crearon dos métodos para manejar la visualización de la información de un usuario.
+    * El primer método recibe un ID como parámetro y se encarga de obtener los detalles del usuario.
+    * Luego, redirige al servicio de visualización de usuario, pasando el usuario obtenido como un atributo de modelo utilizando  @ModelAttribute .
+    * Se optó por este enfoque debido a que, al retornar directamente la plantilla desde el método  verDetalle  con el parámetro ID, los estilos y las imágenes no se mostraban correctamente debido a las restricciones de Spring Security.
+  */
+    @GetMapping("/detalleUsuario/{id}")
+    // @pathVariable sirve para obtener el valor de la variable que se encuentra en la url
+    public String verDetalle(Model model, @PathVariable int id,RedirectAttributes redirectAttributes){
+
+        try {
+            Usuario usuario = usuarioServices.getUsuarioById(id);
+            redirectAttributes.addFlashAttribute("usuario", usuario);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return "redirect:/verUsuario";
+
+    }
+
+    // Por otro lado, el segundo método no requiere parámetros y se encarga de mostrar los estilos y las imágenes correctamente.
+    // Al no tener restricciones adicionales, la plantilla se renderiza adecuadamente con todos los elementos visuales.
+
+    @GetMapping("/verUsuario")
+    public String verUsuario(Model model,@ModelAttribute("usuario") Usuario usuario){
+
+        model.addAttribute("usuario",usuario);
 
         return "users/informationUser";
     }
