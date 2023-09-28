2754a74508b55b67cd5166a68d90022af762c3e8
andres briñez <brinezlopez08@gmail.com>
2023-09-28 11:38:43 -0500

    Se actualiza el controlador para que se pueda guardar la imagen de peril al ser actualizada

--- src/main/java/com/ProyectoIntegrador/sistematransaccionesbancarias/infraestructure/controllers/resources/Usuarios.java
+++ src/main/java/com/ProyectoIntegrador/sistematransaccionesbancarias/infraestructure/controllers/resources/Usuarios.java
@@ -6,9 +6,6 @@ import com.ProyectoIntegrador.sistematransaccionesbancarias.application.services
 import com.ProyectoIntegrador.sistematransaccionesbancarias.domain.entities.Estado;
 import com.ProyectoIntegrador.sistematransaccionesbancarias.domain.entities.Rol;
 import com.ProyectoIntegrador.sistematransaccionesbancarias.domain.entities.Usuario;
-import com.ProyectoIntegrador.sistematransaccionesbancarias.infraestructure.controllers.dto.UsuarioDto;
-import com.ProyectoIntegrador.sistematransaccionesbancarias.infraestructure.data.dbo.EstadoJPAEntity;
-import com.ProyectoIntegrador.sistematransaccionesbancarias.infraestructure.data.dbo.RolJPAEntity;
 import com.ProyectoIntegrador.sistematransaccionesbancarias.infraestructure.data.dbo.UsuarioJPAEntity;
 import com.ProyectoIntegrador.sistematransaccionesbancarias.infraestructure.data.jpaRepositories.Rol.RolImplementacion;
 import com.ProyectoIntegrador.sistematransaccionesbancarias.infraestructure.data.jpaRepositories.estado.EstadoImplementacion;
@@ -23,12 +20,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import static com.ProyectoIntegrador.sistematransaccionesbancarias.infraestructure.controllers.resources.Controller.getUsuarioLogeado;
@@ -188,8 +186,8 @@ public class Usuarios {
     }
 
     @GetMapping("/editarPerfil")
-    public String editarPerfil(Model model,HttpServletRequest request) {
-
+    public String editarPerfil(Model model, HttpServletRequest request ) {
+        
         Usuario usuarioLogeado = getUsuarioLogeado(request); // Se obtiene el usuario que inició sesión
 
         model.addAttribute("usuario",usuarioLogeado);
@@ -198,15 +196,48 @@ public class Usuarios {
     }
 
     @PostMapping("/updateProfile")
-    public String updateProfile(@ModelAttribute("usuario")UsuarioJPAEntity usuario, RedirectAttributes redirectAttributes,HttpServletRequest request) {
+    public String updateProfile(@ModelAttribute("usuario")UsuarioJPAEntity usuario, RedirectAttributes redirectAttributes,@RequestParam("file") MultipartFile imagen) {
 
+        // @RequestParam("file") MultipartFile file -> Se obtiene la imagen del formulario y se guarda en un objeto MultipartFile
+        boolean updateProfile = true;
 
+        // Validaciones de la imagen, si la imagen no está vacia se guarda obtiene la url de la imagen y se guarda en el usuario
+        if(!imagen.isEmpty()){
+
+            System.out.println("Se intenta guardar la imagen");
+
+            Path directorioImagenes = Paths.get("src//main//resources//static//images/profile"); // Se obtiene la ruta de la carpeta donde se guardará la imagen
+            String rutaAbsoluta = directorioImagenes.toFile().getAbsolutePath(); // Se obtiene la ruta absoluta de la carpeta
+
+        // Se intenta guardar la imagen en la carpeta
         try {
-            usuarioServices.UpdateUsuario(mapperUsuario.UsuarioJPAToUsuarioDomain(usuario));
-            redirectAttributes.addFlashAttribute("mensaje", "updateOk");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            redirectAttributes.addFlashAttribute("mensaje", "updateError");
+
+            byte[] bytesImg = imagen.getBytes(); // Se obtienen los bytes de la imagen
+            Path rutaCompleta = Paths.get(rutaAbsoluta + "//" + imagen.getOriginalFilename()); // Se obtiene la ruta completa de la imagen
+            Files.write(rutaCompleta, bytesImg); // Se guarda la imagen en la ruta especificada
+            usuario.setUrlImage(imagen.getOriginalFilename()); // Se guarda la url de la imagen en el usuario que es el nombre de la imagen
+
+            updateProfile = true; // Se puede actualizar el perfil porque se pudo guardar la imagen
+
+            } catch (Exception e) {
+                updateProfile = false; // No se guarda el perfil porque no se pudo guardar la imagen
+                System.out.println(e.getMessage());
+                System.out.println("No se pudo guardar la imagen");
+            }
+        }
+
+        if(updateProfile){
+            // se intenta actualizar el usuario
+            try {
+                System.out.println("Se intenta actualizar el usuario");
+                usuarioServices.UpdateUsuario(mapperUsuario.UsuarioJPAToUsuarioDomain(usuario));
+                redirectAttributes.addFlashAttribute("mensaje", "updateOk");
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                redirectAttributes.addFlashAttribute("mensaje", "updateError");
+            }
+        }else{
+            redirectAttributes.addFlashAttribute("mensaje", "updateErrorImg");
         }
 
         return "redirect:/perfil";
