c184d5c6aac6aa0d3632045526c775bbad9630f8
andres briñez <brinezlopez08@gmail.com>
2023-10-21 15:50:36 -0500

    Se agrega las configuraciones para mostra la documentación

--- src/main/java/com/ProyectoIntegrador/sistematransaccionesbancarias/infraestructure/utils/OpenApiConfig.java
+++ src/main/java/com/ProyectoIntegrador/sistematransaccionesbancarias/infraestructure/utils/OpenApiConfig.java
@@ -12,12 +12,12 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI awesomeAPI() {
         return new OpenAPI()
-                .info(new Info().title("Título de la API")
-                        .description("Descripción de la API")
-                        .version("Versión de la API")
-                        .license(new License().name("Nombre de la licencia").url("URL de la licencia")))
+                .info(new Info().title("Sistema de Transacciones Bancarias Makaia  API")
+                        .description("Este es el backend del Sistema de Transacciones Bancarias, que proporciona servicios para administrar cuentas, bolsillos y transacciones. La API incluye funcionalidades como registro, inicio de sesión, gestión de usuarios para administradores y gestión de perfile para usuarios. También ofrece estadísticas financieras para un mejor control de tus transacciones. Ofrece autenticación y autorización por roles, con usuarios y administradores.")
+                        .version("1.0.0")
+                        .license(new License().name("Licencia MIT").url("https://opensource.org/licenses/MIT")))
                 .externalDocs(new ExternalDocumentation()
-                        .description("Descripción de la documentación externa")
-                        .url("URL de la documentación externa"));
+                        .description("Repositorio de GitHub")
+                        .url("https://github.com/WhitneySt/proyecto-integrador-backend/tree/main"));
     }
 }
