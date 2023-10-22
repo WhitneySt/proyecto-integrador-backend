db9532d492fd9344aef2e7d9b5182182bcc3eae3
andres briñez <brinezlopez08@gmail.com>
2023-10-21 19:45:15 -0500

    Se crea el metodo Query para obtener la cantidad de transacciones

--- src/main/java/com/ProyectoIntegrador/sistematransaccionesbancarias/infraestructure/data/jpaRepositories/transaccion/TransaccionJPARepository.java
+++ src/main/java/com/ProyectoIntegrador/sistematransaccionesbancarias/infraestructure/data/jpaRepositories/transaccion/TransaccionJPARepository.java
@@ -14,4 +14,7 @@ public interface TransaccionJPARepository extends CrudRepository<TransaccionJPAE
 
     @Query(value="SELECT COALESCE(SUM(saldo), 0) FROM transaccion WHERE usuario_id =?1", nativeQuery = true)
     public Double getTotalSaldoTransacciones(Long usuarioId);
+
+    @Query(value="SELECT COUNT(*) FROM transaccion", nativeQuery = true)
+    public Integer getCantidadTransacciones();
 }
